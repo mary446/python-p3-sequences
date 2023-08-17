d31a9eb4857044df66a2a0073519df1c71aa4538
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-17 23:29:20 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = [0, 1]
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        while len(fib) < length:
+            fib.append(fib[-1] + fib[-2])
+        print(fib)
